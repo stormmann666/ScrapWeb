3b97a61ae85107a8251661482b1572eccec466b5
Antonio <srdolar@me.com>
2019-10-22 19:42:31 +0200

    Initial Commit

---
+++ ScrapWeb/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  ScrapWeb
+//
+//  Created by Antonio on 22/10/2019.
+//  Copyright © 2019 Antonio. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var emailTF : UITextField!
+    @IBOutlet weak var passwordTF : UITextField!
+    
+    
+    let webView =  WKWebView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn() {
+        
+        
+        
+        
+    }
+
+
+}
+
---
+++ ScrapWeb/emailsVC.swift
@@ -0,0 +1,50 @@
+//
+//  emailsVC.swift
+//  ScrapWeb
+//
+//  Created by Antonio on 22/10/2019.
+//  Copyright © 2019 Antonio. All rights reserved.
+//
+
+import UIKit
+
+class emailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension emailsVC : UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        
+        
+        
+    }
+    
+}
